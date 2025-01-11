42485d0a9187ef0accf1a7576b68b0516ee7564a
danielpraisethelord <danielyosef777@gmail.com>
2025-01-11 12:16:17 -0600

    First Commit

---
+++ microservice-course/src/main/java/com/microservice/course/service/CourseServiceImpl.java
@@ -0,0 +1,50 @@
+package com.microservice.course.service;
+
+import com.microservice.course.client.StudentClient;
+import com.microservice.course.dto.StudentDTO;
+import com.microservice.course.entities.Course;
+import com.microservice.course.http.response.StudentsByCourseResponse;
+import com.microservice.course.persistence.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CourseServiceImpl implements ICourseService{
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private StudentClient studentClient;
+
+    @Override
+    public List<Course> findAll() {
+        return (List<Course>) courseRepository.findAll();
+    }
+
+    @Override
+    public Course findById(Long id) {
+        return courseRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void save(Course course) {
+        courseRepository.save(course);
+    }
+
+    @Override
+    public StudentsByCourseResponse findStudentsByCourseId(Long courseId) {
+
+        Course course = courseRepository.findById(courseId).orElseThrow();
+
+        List<StudentDTO> studentDTOList = studentClient.findAllStudentsByCourse(courseId);
+
+        return StudentsByCourseResponse.builder()
+                .courseName(course.getName())
+                .teacher(course.getTeacher())
+                .studentsDTOList(studentDTOList)
+                .build();
+    }
+}
---
+++ microservice-student/src/main/java/com/microservice/student/service/IStudentService.java
@@ -0,0 +1,17 @@
+package com.microservice.student.service;
+
+import com.microservice.student.entities.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IStudentService {
+
+    List<Student> findAll();
+
+    Optional<Student> findById(Long id);
+
+    void save(Student student);
+
+    List<Student> findByCourseId(Long courseId);
+}
